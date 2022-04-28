2879c66962de36ac64f120b0c47be83b57eac254
murdakCreations <96468303+murdakCreations@users.noreply.github.com>
2022-04-28 13:12:32 +0800

    Add files via upload

--- raspberryPiCodes/i2c_master.py
+++ raspberryPiCodes/i2c_master.py
@@ -1,9 +1,7 @@
 def i2cComm(prediction):
     from smbus import SMBus
-    categories = ["paper","plastic",  "glass", "metal", "cardboard"]
     addr = 0x8 # bus address
     bus = SMBus(1) # indicates /dev/ic2-1
     value = prediction.item(0)
-    print(type(value))
+    #print(type(value))
     bus.write_byte(addr, value)
-    #print(hex(int(prediction)))
