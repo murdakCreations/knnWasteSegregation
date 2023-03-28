213a1ee7e26bcf4656c2573a6fac24bd277c96cf
murdakCreations <96468303+murdakCreations@users.noreply.github.com>
2023-03-28 21:02:35 +0800

    Add files via upload

--- i2cAndGsmModule/i2cAndGsmModule.ino
+++ i2cAndGsmModule/i2cAndGsmModule.ino
@@ -2,80 +2,48 @@
 #include <Wire.h>
 SoftwareSerial sim(10, 11);
 
-//String categories = ["paper","plastic","glass","metal","cardboard"];
 void setup() {
-  // put your setup code here, to run once:
-  Wire.begin(0x8);
   Serial.begin(9600);
   sim.begin(9600);
   delay(1000);
-  Wire.onReceive(receiveCategory);
-  delay(100);
-  //sim.println("AT+CMEE=1");
-  //delay(10000);
 }
 
 void loop() {
+  testGSM();
+  delay(200);
+  //Serial.println("\n200 msec");
+  /*testGSM2();
+  displayStat();
+  delay(10000);
+  Serial.println("10 sec");
   
-  // put your main code here, to run repeatedly:
-  /*if (Serial.available() > 0)
-    switch (Serial.read())
-    {
-      case 't':
-        testGSM();
-        break;
-      case 's':
-        testGSM2();
-        break;
-      case 'r':
-        sendSMS();
-        break;
-    }
-  
-  if (sim.available() > 0)
-  Serial.write(sim.read());*/
+  /*sendSMS();
+  delay(10000);
+  displayStat();*/
+}
+
+void displayStat(){
+  if (sim.available() > 0){
+  Serial.write(sim.read());}
 }
 
 void testGSM(){
-  sim.println("AT+CMGF=1");
+  sim.write("\nAT+CMGF=1\n");
   delay(200);
+  displayStat();
+  
 }
 
 void testGSM2(){
-  sim.println("AT+CMGF=0");
-  delay(200);
+  sim.println("\nAT+CMGF=0\n");
+  delay(60000);
 }
 
 void sendSMS(){
-  sim.write("AT+CMGS=\"+639267035052\"\r\n");
-  delay(100);
-  sim.write("aaa");
-  delay(100);
+  sim.write("\nAT+CMGS=\"+639355667198\"\r\n");
+  delay(6000);
+  sim.write("hello there\n The bin is full");
+  delay(6000);
   sim.write((char)26);
-  delay(100); 
-}
-
-void receiveCategory(int byteCounts){
-  while(Wire.available()){// loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    c = c + '0';
-    if (c == '0'){
-      Serial.print('paper');     
-    }
-    else if (c == '1'){
-      Serial.print('plastic');     
-    }
-    else if (c == '2'){
-      Serial.print('glass');     
-    }
-    else if (c == '3'){
-      Serial.print('metal');     
-    }
-    else if (c == '4'){
-      Serial.print('cardboard');     
-    }
-
-    // add '0' to convert int to char
-    Serial.print(c);
-  }
+  delay(6000); 
 }
