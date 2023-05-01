2d608297534447383fc5055e1b1c94c13140d694
murdakCreations <96468303+murdakCreations@users.noreply.github.com>
2023-05-01 12:19:13 +0800

    before formatting raspberry pi

--- raspberryPiCodes/structure.py
+++ raspberryPiCodes/structure.py
@@ -1,7 +1,15 @@
-# take picture
-import takePic
-takePic.takePic()
+while 1:
 
-# train algorithm
-import trainAlgo
-trainAlgo.trainKnn()
+    # library for delay
+    import time
+
+    # take picture
+    import takePic
+    takePic.takePic()
+
+    # train algorithm
+    import trainAlgo
+    trainAlgo.trainKnn()
+
+    # 10 seconds delay
+    time.sleep(10)
